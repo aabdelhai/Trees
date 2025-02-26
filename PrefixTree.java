db346f93e7c1af923e4f0837db54991772b207b7
aabdelhai <157061599+aabdelhai@users.noreply.github.com>
2025-02-25 19:03:07 -0600

    Add files via upload

---
+++ PrefixTree.java
@@ -0,0 +1,95 @@
+package autoComplete;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * A prefix tree used for autocompletion. The root of the tree just stores links to child nodes (up to 26, one per letter).
+ * Each child node represents a letter. A path from a root's child node down to a node where isWord is true represents the sequence
+ * of characters in a word.
+ */
+public class PrefixTree {
+    private TreeNode root; 
+
+    // Number of words contained in the tree
+    private int size;
+
+    public PrefixTree(){
+        root = new TreeNode();
+    }
+
+    /**
+     * Adds the word to the tree where each letter in sequence is added as a node
+     * If the word, is already in the tree, then this has no effect.
+     * @param word
+     */
+    public void add(String word){
+        TreeNode current = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            current.children.putIfAbsent(ch, new TreeNode());
+            current = current.children.get(ch);
+            current.letter = ch;
+        }
+        if(!current.isWord){
+            current.isWord = true; 
+                size++;
+        }
+    }
+
+    /**
+     * Checks whether the word has been added to the tree
+     * @param word
+     * @return true if contained in the tree.
+     */
+    public boolean contains(String word){
+        TreeNode current = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if(!current.children.containsKey(ch)){
+                return false;
+            }
+            current = current.children.get(ch);
+        }
+        return current.isWord;
+    }
+
+    /**
+     * Finds the words in the tree that start with prefix (including prefix if it is a word itself).
+     * The order of the list can be arbitrary.
+     * @param prefix
+     * @return list of words with prefix
+     */
+    public ArrayList<String> getWordsForPrefix(String prefix){
+        ArrayList<String> result = new ArrayList<>();
+        TreeNode current = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            current = current.children.get(ch);
+            if(current == null){
+                return result;
+            }
+    }
+    prefix(current, new StringBuilder(prefix), result);
+    return result;
+}
+
+    private void prefix(TreeNode node, StringBuilder pre, ArrayList<String> result){
+        if(node.isWord){
+            result.add(pre.toString());
+        }
+        for (Map.Entry<Character,TreeNode> entry : node.children.entrySet()) {
+            pre.append(entry.getKey());
+            prefix(entry.getValue(), pre, result);
+            pre.deleteCharAt(pre.length() - 1);
+        }
+    }
+
+    /**
+     * @return the number of words in the tree
+     */
+    public int size(){
+        return size;
+    }
+    
+}
